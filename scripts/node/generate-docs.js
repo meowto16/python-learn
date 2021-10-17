1a4791db0df8886b6fb1b5eba6fea92b4badc12c
Maxim Kirshin <mak@crtweb.ru>
2021-10-17 15:57:56 +0500

    feat: add navigation for docs and headings

--- scripts/node/generate-docs.js
+++ scripts/node/generate-docs.js
@@ -3,8 +3,11 @@ const fs = require('fs')
 
 const PROJECT_ROOT_PATH = path.resolve(__dirname, '..', '..')
 const DOCS_PATH = path.resolve(PROJECT_ROOT_PATH, 'docs')
+const README_PATH = path.resolve(PROJECT_ROOT_PATH, 'README.md')
 const VIDEOS_PATH = path.resolve(PROJECT_ROOT_PATH, '..', 'Videos', 'python-learn')
 const WATCHED_EXTENSIONS = ['.mp4', '.html']
+
+fs.appendFileSync(README_PATH, `# Python learn \n\n`)
 
 const videosDirectories = getDirectories(VIDEOS_PATH)
 const lessons = videosDirectories.map((dir) => {
@@ -19,17 +22,21 @@ const lessons = videosDirectories.map((dir) => {
   }
 })
 
-lessons.forEach((lesson) => {
+lessons.forEach(async (lesson) => {
   const lessonDirPath = path.join(DOCS_PATH, lesson.name)
+  const encodedLessonDirRelativePath = `./docs/${encodeURIComponent(lesson.name)}`
   fs.mkdirSync(lessonDirPath)
+  fs.appendFileSync(README_PATH, `- [${lesson.name}](${encodedLessonDirRelativePath})\n`)
 
   lesson.videos.forEach((lessonName) => {
-    fs.writeFile(path.resolve(lessonDirPath, `${lessonName}.md`), '', function (err) {
+    const lessonFileName = `${lessonName}.md`
+    const encodedLessonFileName = encodeURIComponent(lessonFileName)
+    fs.writeFileSync(path.resolve(lessonDirPath, `${lessonName}.md`), `# ${lessonName}\n\n`, function (err) {
       if (err) throw err;
     });
+    fs.appendFileSync(README_PATH, `  - [${lessonName}](${encodedLessonDirRelativePath}/${encodedLessonFileName})\n`)
   })
 })
-
 
 function getDirectories (source) {
   return fs.readdirSync(source, { withFileTypes: true })
